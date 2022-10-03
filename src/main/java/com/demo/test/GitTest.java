cfcdb4b87be86527bee0cbbc4cd73ee0c9493fd6
jian <1330968116@qq.com>
2022-10-03 15:13:33 +0800

    first  commit

---
+++ src/main/java/com/demo/test/GitTest.java
@@ -0,0 +1,15 @@
+package com.demo.test;
+
+/**
+ * @author admin
+ * @version 1.0.0
+ * @ClassName GitTest.java
+ * @ddescription
+ * @createTime 2022年10月03日 15:10:00
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
