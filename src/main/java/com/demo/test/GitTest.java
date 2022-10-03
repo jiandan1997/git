7a8c1baad4fa1b3a5b20b2932f2fe23ebb298f20
jian <1330968116@qq.com>
2022-10-03 15:25:50 +0800

    hot-fix commit2

--- src/main/java/com/demo/test/GitTest.java
+++ src/main/java/com/demo/test/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git hot-fix");
     }
 }
